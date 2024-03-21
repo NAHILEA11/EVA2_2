c4f741ead4ce29cda8c628918bd256371c56e30e
NAHILEA11 <159495242+NAHILEA11@users.noreply.github.com>
2024-03-21 16:26:23 -0600

    Add files via upload

---
+++ EVA2_16_SWITCH.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_16_switch;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class EVA2_16_SWITCH {
+
+    public static void main(String[] args) {
+        //TODO code aaplication logic here
+        int mes;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Introduce el numero del mes: ");
+        mes = captu.nextInt();
+        
+        switch(mes){
+            case 1:
+                System.out.println("Enero");
+                break;//termina el switch
+            case 2:
+                System.out.println("Febrero");
+                break;//termina el switch
+            case 3:
+                System.out.println("Marzo");
+                break;//termina el switch
+            case 4:
+                System.out.println("Abril");
+                break;//termina el switch
+            case 5:
+                System.out.println("Mayo");
+                break;//termina el switch
+            case 6:
+                System.out.println("Junio");
+                break;//termina el switch
+            case 7:
+                System.out.println("Julio");
+                break;//termina el switch
+             case 8:
+                System.out.println("Agosto");
+                break;//termina el switch
+             case 9:
+                System.out.println("Septiembre");
+                break;//termina el switch
+             case 10:
+                System.out.println("Octubre");
+                break;//termina el switch
+             case 11:
+                System.out.println("Noviembre");
+                break;//termina el switch
+             case 12:
+                System.out.println("Diciembre");
+                break;//termina el switch
+                
+             
+                
+             
+    }
+}            case 1 -> System.out.println("Enero");
+            //termina el switch
+            case 2 -> System.out.println("Febrero");
+            //termina el switch
+            case 3 -> System.out.println("Marzo");
+            //termina el switch
+            case 4 -> System.out.println("Abril");
+            //termina el switch
+            case 5 -> System.out.println("Mayo");
+            //termina el switch
+            case 6 -> System.out.println("Junio");
+            //termina el switch
+            case 7 -> System.out.println("Julio");
+            //termina el switch
+             case 8 -> System.out.println("Agosto");
+            //termina el switch
+             case 9 -> System.out.println("Septiembre");
+            //termina el switch
+             case 10 -> System.out.println("Octubre");
+            //termina el switch
+             case 11 -> System.out.println("Noviembre");
+            //termina el switch
+             case 12 -> System.out.println("Diciembre");
+            //termina el switch
+                
+             
+                
+             
+    }
+}
