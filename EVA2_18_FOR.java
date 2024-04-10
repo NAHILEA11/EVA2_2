43abff228ea5d27fe4de3a141efe945286895b48
NAHILEA11 <159495242+NAHILEA11@users.noreply.github.com>
2024-04-10 07:07:50 -0600

    Add files via upload

---
+++ EVA2_18_FOR.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_18_for;
+
+/**
+ *
+ * @author Nahilea
+ */
+public class EVA2_18_FOR {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+        //INICIO; CONDICION; INCREMENTO/DECREMENTO
+        for(int i = 1; i <= 10; i++){
+            //LLAVES SON OPCIONALES, USENLAS CUANDO
+            //TENGAN MAS DE UNA INSTRUCCION A REPETIR
+            System.out.print(i + " - ");
+        }  
+        System.out.print("");
+        for(int i = 10; i >= 1; i--){
+            //LLAVES SON OPCIONALES, USENLAS CUANDO
+            //TENGAN MAS DE UNA INSTRUCCION A REPETIR
+            System.out.print(i + " - ");
+        }
+        //IMPRIMIR LOS NUMEROS PARES ENTRE 0 Y 100
+        //0, 2  4 6 8 10
+        System.out.println("");
+        for(int i = 0; i <= 100; i+=2){
+            System.out.print(i + " - ");
+    
+            }
+        
+            
+            
+        }
+    }
+
