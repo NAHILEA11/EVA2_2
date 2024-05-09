b1535e41ad90a23350641f0df289b40614531050
NAHILEA11 <159495242+NAHILEA11@users.noreply.github.com>
2024-05-09 16:42:17 -0600

    Add files via upload

---
+++ EVA2_17_BISIESTO0.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_17_bisiesto0;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nahilea
+ */
+public class EVA2_17_BISIESTO0 {
+
+    public static void main(String[] args) {
+        //TODO code application logic here
+        int anio;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Introduce el año a evaluar : ");
+        anio = captu.nextInt();
+    }
+}
